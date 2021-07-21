3b149337e872b6907fdff2487f12cddf45e9e97f
Ykoh <ykoh@student.42seoul.kr>
2021-07-21 17:31:05 +0900

    Feat: config 설정

---
+++ 03-Data-persistence/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+  ],
+})
+export class AppModule {}
